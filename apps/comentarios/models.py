679f013df35385fff7f0296a7194b43ec5baeb15
Csosafloriani <florianicarlosismael@gmail.com>
2024-10-01 21:13:42 -0300

    Refactorizacion de modelos: agregado campo autor a Noticia, movido el modelo comentario a la app comentarios

--- apps/comentarios/models.py
+++ apps/comentarios/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+from apps.usuarios.models import Usuario
+from apps.noticias.models import Noticia
+
 # Create your models here.
+class Comentario(models.Model):
+	usuario = models.ForeignKey(Usuario, on_delete = models.CASCADE)
+	texto = models.TextField(max_length = 1500)
+	noticia = models.ForeignKey(Noticia, on_delete = models.CASCADE)
+	fecha = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return f"{noticia}->{texto}"
--- apps/noticias/views.py
+++ apps/noticias/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 
 from django.contrib.auth.decorators import login_required
 
-from .models import Noticia, Categoria, Comentario
+from .models import Noticia, Categoria
+from apps.comentarios.models import Comentario
 
 from django.urls import reverse_lazy
 
