0c1ecb739cb31caf49fd186fe537e9892da2dfcc
Nancy Debárbora <nancydebarbora@yahoo.com.ar>
2024-10-03 11:23:59 -0300

    cambios en base.html

--- apps/usuarios/models.py
+++ apps/usuarios/models.py
@@ -3,4 +3,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Usuario(AbstractUser):
-	pass
+	ROLES= (
+		('no_registrado', 'No Registrado'),
+		('registrado', 'Registrado'),
+		('admin', 'Administrador'),
+	)
+
+
+rol = models.CharField(max_length=20, choices='ROLES', default='registrado') 
