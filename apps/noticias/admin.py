d3a4308c260436b74a6b6d25c9fb9282b5180676
Csosafloriani <florianicarlosismael@gmail.com>
2024-10-15 01:02:46 -0300

    correcciones lugar enlace denuncia; registro del model en panel admin

--- apps/noticias/admin.py
+++ apps/noticias/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Categoria, Noticia
+from .models import Categoria, Noticia, Denuncia
 
 class  CategoriaAdmin(admin.ModelAdmin):
     list_display=['nombre']
@@ -10,5 +10,12 @@ class NoticiaAdmin(admin.ModelAdmin):
     list_display=['titulo', 'autor', 'cuerpo', 'imagen', 'categoria_noticia', 'fecha']
     search_fields=['titulo',]
 
+class DenunciaAdmin(admin.ModelAdmin):
+    list_display = ('noticia', 'usuario', 'motivo', 'fecha')  
+    list_filter = ('motivo', 'fecha')  
+    search_fields = ('usuario__username', 'noticia__titulo')  
+
+
+admin.site.register(Denuncia, DenunciaAdmin)
 admin.site.register(Categoria, CategoriaAdmin)#puse en el admin
 admin.site.register(Noticia, NoticiaAdmin)#puse en el admin
