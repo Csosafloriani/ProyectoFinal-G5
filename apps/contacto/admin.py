8d3e3cc5c580b08b93df53dded4dd840a624f6b5
Csosafloriani <florianicarlosismael@gmail.com>
2024-10-17 10:19:41 -0300

    correccion sintaxis apps/contacto/admin.py

--- apps/contacto/admin.py
+++ apps/contacto/admin.py
@@ -5,7 +5,7 @@ from .models import Contacto
 
 
 class ContactoAdmin(admin.ModelAdmin):
-    list_display=('id','nombre_apellido', 'email', 'aunto', 'fecha',)
+    list_display=('id','nombre_apellido', 'email', 'asunto', 'fecha',)
     search_fields=['id','nombre_apellido', 'email', 'asunto', 'fecha',]# busca por todo
 
 
